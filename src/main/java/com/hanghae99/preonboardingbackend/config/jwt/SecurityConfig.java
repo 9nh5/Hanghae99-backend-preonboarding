cdb4e1fbeaecc038851f7fdd128543ed02cd19a8
9nh5 <koon6756@naver.com>
2024-07-09 21:17:42 +0900

    feat/day-2 : Securtiy및 Swagger Config 작성
    Swagger, Security Config 작성 및 AccessToken 발행 및 검증 코드 작성

--- src/main/java/com/hanghae99/preonboardingbackend/config/jwt/SecurityConfig.java
+++ src/main/java/com/hanghae99/preonboardingbackend/config/jwt/SecurityConfig.java
@@ -1,12 +1,39 @@
 package com.hanghae99.preonboardingbackend.config.jwt;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final TokenProvider tokenProvider;
+
+    private static final String[] allowedurls = {"/sign-up", "/sign-in"};
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(CsrfConfigurer<HttpSecurity>::disable)
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+                .authorizeHttpRequests(requests ->
+                        requests.requestMatchers(allowedurls).permitAll()
+                                .requestMatchers(PathRequest.toH2Console()).permitAll()
+                                .anyRequest().authenticated()
+                )
+                .sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .build();
+    }
+
+
 }
--- src/main/java/com/hanghae99/preonboardingbackend/config/jwt/TokenProvider.java
+++ src/main/java/com/hanghae99/preonboardingbackend/config/jwt/TokenProvider.java
@@ -1,11 +1,19 @@
 package com.hanghae99.preonboardingbackend.config.jwt;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
+import javax.crypto.spec.SecretKeySpec;
 import java.security.Key;
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
 
 @Configuration
 @Slf4j
@@ -15,10 +23,31 @@ public class TokenProvider implements InitializingBean {
     private String secret;
     @Value("${jwt.token-validity-in-milliseconds}")
     private long tokenValidityInMilliseconds;
+    @Value("${jwt.issuer}")
+    private String issuer;
     private Key key;
 
     @Override
     public void afterPropertiesSet() throws Exception {
 
+    }
+
+    public String createAccessToken(String userSpecification) {
+        return Jwts.builder()
+                .signWith(new SecretKeySpec(secret.getBytes(), SignatureAlgorithm.HS512.getJcaName()))
+                .setSubject(userSpecification)
+                .setIssuer(issuer)
+                .setIssuedAt(Timestamp.valueOf(LocalDateTime.now()))
+                .setExpiration(Date.from(Instant.now().plus(tokenValidityInMilliseconds, ChronoUnit.MILLIS)))
+                .compact();
+    }
+
+    public String validateTokenAndGetSubject(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(secret.getBytes())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
     }
 }
